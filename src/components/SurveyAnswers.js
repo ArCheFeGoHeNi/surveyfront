f9da3987cd7619244f983d2faa0bef84f684fb9d
FeeliksKilpi <feelikskilpi@outlook.com>
2020-05-07 12:38:04 +0300

    Merged with previous commit by Gosols

--- src/components/SurveyAnswers.js
+++ src/components/SurveyAnswers.js
@@ -2,15 +2,11 @@ import React, {useState, useEffect} from 'react'
 import Card from '@material-ui/core/Card'; 
 import { useParams } from "react-router";
 
-
-
 export default function SurveyAnswers () {
 const  [answers, setAnswers] = useState([]); 
 const [questions, setQuestions] = useState([]); 
 
 const  [msg, setMsg] = useState('Loading jalludata...'); 
-
-
 
 React.useEffect(() => {
     fetch(`https://surveyapp-backend.herokuapp.com/surveyslist/2`)
@@ -25,14 +21,9 @@ React.useEffect(() => {
 
   console.log(questions)
 
-
-
 return ( 
     <div> 
         <Card> 
-
-           
-
             {answers.map((adata) => ( 
 
             <p key=             { adata.questionId}> 
@@ -40,13 +31,7 @@ return (
                 { adata.answer.answerID}
                 { adata.answerText}
             
-           
             {console.log(adata)}
-          
-          
-                
-            
-
             </p>
             )) }
 
